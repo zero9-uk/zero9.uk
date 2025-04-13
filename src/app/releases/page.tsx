c2ddb37cc955cc139d929982f4411fbe763f1c3d
James Harcourt <jharcourt@bionics.co.uk>
2025-04-13 15:59:25 +0100

    Releases page work

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -8,39 +8,36 @@ type Release = {
 
 const releases: Release[] = [
   {
-    id: 'release1',
-    title: 'ZERO9001',
-    image: '/releases/zero9001.png',
-  },
+    id: 'release3',
+    title: 'ZERO9003',
+    image: '/releases/zero9003.png',
+  },  
   {
     id: 'release2',
     title: 'ZERO9002',
     image: '/releases/zero9002.png',
   },
   {
-    id: 'release3',
-    title: 'ZERO9003',
-    image: '/releases/zero9003.png',
+    id: 'release1',
+    title: 'ZERO9001',
+    image: '/releases/zero9001.png',
   },
 ];
 
 export default function ReleasesPage() {
   return (
-    <div className="pt-[80px] px-6 pb-12 bg-white min-h-screen text-black">
+    <div className="pt-[80px] px-6 pb-12 bg-white min-h-screen text-black flex justify-center">
       <div
-        className={`grid ${
+        className={`grid gap-8 w-full max-w-[1400px] ${
           releases.length === 1
-            ? 'grid-cols-1 place-items-center'
+            ? 'grid-cols-1'
             : releases.length === 2
-            ? 'grid-cols-1 sm:grid-cols-2 place-items-center'
+            ? 'grid-cols-1 sm:grid-cols-2'
             : 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3'
-        } gap-8`}
+        }`}
       >
         {releases.map((release) => (
-          <div
-            key={release.id}
-            className="flex flex-col items-center w-full max-w-[400px]"
-          >
+          <div key={release.id} className="flex flex-col items-center w-full">
             <div className="relative bg-[#f7f7f7] p-[3px] aspect-square w-full">
               <img
                 src={release.image}
