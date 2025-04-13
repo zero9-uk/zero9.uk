2a16276cb5ee4c9a63d8b3e73ff21a8c16dc6dd0
James Harcourt <jharcourt@bionics.co.uk>
2025-04-13 15:46:46 +0100

    Work on display of releases page/grid

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -22,21 +22,6 @@ const releases: Release[] = [
     title: 'ZERO9003',
     image: '/releases/zero9003.png',
   },
-  {
-    id: 'release1',
-    title: 'ZERO9001',
-    image: '/releases/zero9001.png',
-  },
-  {
-    id: 'release2',
-    title: 'ZERO9002',
-    image: '/releases/zero9002.png',
-  },
-  {
-    id: 'release3',
-    title: 'ZERO9003',
-    image: '/releases/zero9003.png',
-  },
 ];
 
 export default function ReleasesPage() {
